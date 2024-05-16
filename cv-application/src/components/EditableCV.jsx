e0ca64c68db390b25bdc6c1acd3296808eb5f1be
DJacobson17 <83249575+DJacobson17@users.noreply.github.com>
2024-05-16 12:30:39 -0500

    added education section

--- cv-application/src/components/EditableCV.jsx
+++ cv-application/src/components/EditableCV.jsx
@@ -5,11 +5,12 @@ function EditableCV() {
     <>
         <GeneralInfo {...person} />
         <Experience {...person} />
+        <Education {...person} />
     </>
   )
 }
 
-const person = { name: 'John Doe', email: 'johndoe@gmail.com', phone: '555-555-5555', jobs: [{ id: 1, title: 'Software Engineer', company: 'Google', dates: '2018-2021', responsibilities: ['Worked on the Google search engine.', 'Managed a team of 10.'] }, { id: 2, title: 'Software Engineer', company: 'Facebook', dates: '2015-2018', responsibilities: ['Worked on the Facebook social network.', 'Managed a team of 5 developers.']}]}
+const person = { name: 'John Doe', email: 'johndoe@gmail.com', phone: '555-555-5555', jobs: [{ id: 1, title: 'Software Engineer', company: 'Google', dates: '2018-2021', responsibilities: ['Worked on the Google search engine.', 'Managed a team of 10.'] }, { id: 2, title: 'Software Engineer', company: 'Facebook', dates: '2015-2018', responsibilities: ['Worked on the Facebook social network.', 'Managed a team of 5 developers.']}], education: [{ id: 1, degree: 'BS Computer Science', school: 'MIT', dates: '2011-2015', location: 'Cambridge, MA' }, { id: 2, degree: 'MS Computer Science', school: 'Stanford', dates: '2015-2017', location: 'Palo Alto, CA' }]}
 
 function GeneralInfo(person) {
   return (
@@ -41,6 +42,26 @@ function Experience(person) {
                   </li>
                 )}
               </li>
+          )}        
+        </ul>
+      </section>
+    </>
+  )
+}
+
+function Education(person) {
+  return (
+    <>
+      <section className="education">
+        <h2>Education</h2>
+        <ul>
+          {person.education.map(edu =>
+              <li key={edu.id}>
+              <h3>{edu.degree}</h3>
+              <p>{edu.dates}</p>
+              <h3>{edu.school}</h3>
+              <p>{edu.location}</p>
+            </li>
           )}        
         </ul>
       </section>
