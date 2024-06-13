5786085af44ce0d5f3fe6b9ca9ed58f351003450
DJacobson17 <83249575+DJacobson17@users.noreply.github.com>
2024-06-13 16:14:44 -0500

    styled editing area and added education portion

--- cv-application/src/components/EditArea.jsx
+++ cv-application/src/components/EditArea.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import { useState } from 'react'
+import React, { useState } from 'react'
 import App from '../App'
+import '../styles/editArea.css'
 
 export default function EditArea({section}) {
   if (section == 'general-info') {
@@ -9,16 +9,21 @@ export default function EditArea({section}) {
   else if (section == 'experience') {
     return <ExperienceEdit />
   }
+  else if (section == 'education') {
+    return <EducationEdit />
+  }
 
   function GeneralInfoEdit({...person}) {
     return (
       <div>
         <h2>General Info Edit</h2>
-        <label>Name:</label>
-        <input type="text" value={person.name} />
-        <input type="text" value={person.email} />
-        <input type="text" value={person.phone} />
-        <button>Save</button>
+        <form action="">
+        <input type="text" value={person.name} placeholder='Name' />
+        <input type="text" value={person.email} placeholder='Email' />
+        <input type="text" value={person.phone} placeholder='Phone Number'/>
+        
+        <button type="submit">Submit</button>
+        </form>
       </div>
     )
   }
@@ -26,17 +31,43 @@ export default function EditArea({section}) {
   function ExperienceEdit() {
     return (
       <div>
-        <h2>Experience Edit</h2>
-        <label>Title:</label>
-        <input type="text" />
-        <label>Company:</label>
-        <input type="text" />
-        <label>Dates:</label>
-        <input type="text" />
-        <label>Responsibilities:</label>
-        <input type="text" />
-        <button className='responsibilty'>Add Responsibility</button>
-        <button>Save</button>
+        <h2>Work History Edit</h2>
+        <form action="">
+          <label for='jobTitle'>Job Title</label>
+          <input type="text" id='jobTitle' name='jobTitle' />
+          <label>Company:</label>
+          <input type="text" />
+          <label>Dates:</label>
+          <input type="text" />
+          <label>Responsibilities:</label>
+          <input type="text" />
+          <button className='responsibilty-add'>Add Responsibility</button>
+          <button className='responsibility-delete'>Delete Responsibility</button>
+          <button className="add-job">Add Job</button>
+          <button className="delete-job">Delete Job</button>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    )
+  }
+
+  function EducationEdit() {
+    return (
+      <div>
+        <h2>Education Edit</h2>
+        <form action="">
+          <label for='degree'>Degree:</label>
+          <input type="text" id='degree' name='degree' />
+          <label for='school'>School:</label>
+          <input type="text" id='school' name='school' />
+          <label for='dates'>Dates:</label>
+          <input type="text" id='dates' name='dates' />
+          <label for='location'>Location:</label>
+          <input type="text" id='location' name='location'/>
+          <button className="add-education">Add Education</button>
+          <button className="delete-education">Delete Education</button>
+          <button type="submit">Submit</button>
+        </form>
       </div>
     )
   }
